a90a582378441514dc2447f94661b75104ca208e
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2024-12-05 21:00:13 +0100

    home page

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,10 +1,50 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ headerShown: false }} />
-      <Tabs.Screen name="overview" options={{ headerShown: false }} />
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "black",
+          height: 60,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+          color: "#333",
+        },
+        tabBarActiveTintColor: "#007bff",
+        tabBarInactiveTintColor: "#6c757d",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="overview"
+        options={{
+          tabBarLabel: "Overview",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "list" : "list-outline"}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
--- client/app/(tabs)/home.tsx
+++ client/app/(tabs)/home.tsx
@@ -1,19 +1,108 @@
-import { Colors } from "@/constants/Colors";
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Colors } from "@/constants/Colors";
 
-export class home extends Component {
+type State = {
+  selectedMood: string; // Emoji character
+  moodDescription: string; // Description of the selected mood
+};
+
+export class Home extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      selectedMood: "😊",
+      moodDescription: "Happy",
+    };
+  }
+
+  changeMood = (moodEmoji: string, moodDescription: string) => {
+    this.setState({ selectedMood: moodEmoji, moodDescription });
+  };
+
   render() {
+    const { selectedMood, moodDescription } = this.state;
+
+    const handleButtonClick = () => {
+      Alert.alert("click");
+    };
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}> Welcome back! </Text>
-        <View style={styles.moodPicker}></View>
+        <View style={styles.moodPicker}>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 24,
+              color: Colors.colors.secondary,
+            }}
+          >
+            How are you feeling today?
+          </Text>
+
+          <View style={styles.mainMoodContainer}>
+            <Text style={styles.mainMood}>{selectedMood}</Text>
+            <Text style={styles.moodDescription}>{moodDescription}</Text>
+          </View>
+
+          <View style={styles.emojiPicker}>
+            <TouchableOpacity
+              onPress={() => this.changeMood("😡", "Angry")}
+              style={styles.emojiContainer}
+            >
+              <Text style={styles.emoji}>😡</Text>
+              <Text style={styles.emojiLabel}>Angry</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => this.changeMood("😢", "Sad")}
+              style={styles.emojiContainer}
+            >
+              <Text style={styles.emoji}>😢</Text>
+              <Text style={styles.emojiLabel}>Sad</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => this.changeMood("😐", "Neutral")}
+              style={styles.emojiContainer}
+            >
+              <Text style={styles.emoji}>😐</Text>
+              <Text style={styles.emojiLabel}>Neutral</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => this.changeMood("😊", "Happy")}
+              style={styles.emojiContainer}
+            >
+              <Text style={styles.emoji}>😊</Text>
+              <Text style={styles.emojiLabel}>Happy</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => this.changeMood("😄", "Great Mood")}
+              style={styles.emojiContainer}
+            >
+              <Text style={styles.emoji}>😄</Text>
+              <Text style={styles.emojiLabel}>Very Happy</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
+            <Text style={styles.buttonText}>Set your mood</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
-export default home;
+export default Home;
 
 const styles = StyleSheet.create({
   container: {
@@ -21,17 +110,70 @@ const styles = StyleSheet.create({
     paddingVertical: 24,
     paddingHorizontal: 8,
     padding: 8,
-    position: "relative",
-    // backgroundColor: "#ad5389",
-    // backgroundImage: "linear-gradient(to right, #3c1053, #ad5389)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ad5389",
   },
   welcome: {
     color: Colors.colors.secondary,
     fontSize: 42,
     marginBottom: 6,
+    textAlign: "center",
+    fontWeight: "600",
   },
   moodPicker: {
     height: "60%",
-    backgroundColor: "rgba(255,255,255,.2)",
+  },
+  mainMoodContainer: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  mainMood: {
+    fontSize: 150,
+    textAlign: "center",
+  },
+  moodDescription: {
+    fontSize: 18,
+    color: Colors.colors.secondary,
+    marginTop: 10,
+    textAlign: "center",
+  },
+  emojiPicker: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 12,
+    width: "100%",
+    paddingHorizontal: 16,
+  },
+  emojiContainer: {
+    alignItems: "center",
+  },
+  emoji: {
+    fontSize: 28,
+  },
+  emojiLabel: {
+    fontSize: 14,
+    color: Colors.colors.secondary,
+    textAlign: "center",
+    marginTop: 5,
+  },
+  button: {
+    backgroundColor: "#FF6347", // Tomato color for the button
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "600",
   },
 });
